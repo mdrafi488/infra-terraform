159d974fba06d8de5b8ba9fc96261fa8e21650a4
mdrafi488 <mdrafi.f488@gmail.com>
2022-09-10 16:41:55 +0530

    First commit instances

---
+++ apache.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "apache" {
+  name        = "apache-sg"
+  description = "Allow end user with http"
+  vpc_id      = "vpc-049f215e85a9ff2c2"
+
+  ingress {
+    description      = "connecting end user on Http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "connecting Admin on SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "apache-sg"
+    terrsform ="true"
+  }
+}
+resource "aws_instance" "apache" {
+  ami           = "ami-0b89f7b3f054b957e"
+  instance_type = "t2.micro"
+
+  subnet_id = "subnet-072762878afe76c41"
+  vpc_security_group_ids=[aws_security_group.apache.id]
+  #key_name="test"
+  key_name = aws_key_pair.demo.id
+
+  user_data = file(jenkins-install.sh)
+
+  tags = {
+    Name = "apache"
+  }
+}
---
+++ keypair.tf
@@ -0,0 +1,6 @@
+resource "aws_key_pair" "demo" {
+  key_name   = "demo"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkvl8V/LfODZi0HLmMcmhtcCmjBPzajUvqgW2zKC01uA44Wwp/fTY6JCmkPPsDfaoZKZsrRL9bsfxdW2QgtUcwNWWAmUZBeZbpgErZKlWZUqNn1D0W21cfQ01+uRGdLMoLfsU5hlu6jAaNnuxv56ouDWqroi5XE33WL60CnfvnWRtHs+FOtnYT1J7ih0zu/KYMEz40c7V4SqVsFEOHmUFE+zyBIILp+0CG7ykLvPFPQ4UTFqM44uxGlhNzszGDgQruUZ9DJVDyyk6MeKDp/5+Ezy0nVG64uGHLGkFW2Fx1xenNDZF8auBEhglopEZBESuqDsimEubZLPhfYMf8zgGBUZD4IxcW41+rFe7LCUlYLaEa7eRDD86IAGkqiCgngFUYtsvGdP9Wy2DBRTadJOltJRtzcTUWyTIl69ZhzdAdpwanDr0fwsCSIRBbc8kddD1FdZdPl60SwizmWwf07PbHQCmW1sVW+oek5BCWOQWO+fTqoyrowbEOeCvKIef08ZE= Mr@Mr-PC"
+  #public_key = file(id_rsa.pub)
+  
+}
