151b41698056425217b7daa92d94ff5473bd366d
mdrafi488 <mdrafi.f488@gmail.com>
2022-09-16 23:02:54 +0530

    commited 16-sep

--- cicd.tf
+++ cicd.tf
@@ -44,9 +44,9 @@ resource "aws_security_group" "cicd-sg" {
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum upgrade
+yum upgrade -y
 amazon-linux-extras install java-openjdk11
-yum install jenkin
+yum install jenkins -y
 systemctl start jenkins
 systemctl enable jenkins
               EOF
